027b1c10bef0e86a1c9395ab9edd5c2ab2eddce3
Sumetjutha <sumet.jutha@gmail.com>
2022-06-18 20:44:01 +0700

    update

--- ep54.py
+++ ep54.py
@@ -2,11 +2,41 @@
 
 ########################################################################################################################
 ## สร้าง ฟังก์ชันเพื่อเปรียบเทียบตัวเลข
-def compare(x,y):
+def compareMax(x,y):
     if x>y:
         return x
+    else:
+        return y
 
+# max = compareMax(10,5)
+max = compareMax(10,20)
+print("ค่ามากสุด => ",max)
 
-comnpare(10,5)
+# def equal(x,y,z):
+#     # เปรียบเทียบ x กับ y ก่อน
+#     a = compareMax(x,y)
+#     # เปรียบเทียบกับ z
+#     m = compareMax(a,z)
+#     return m
+
+# เขียนแบบนี้ function ซ้อน function ได้มากกว่า
+def equal(x,y,z):
+    return compareMax(compareMax(x,y),z)
+
+max = equal(10,20,30)
+print("ค่ามากสุด => ",max)
+
+max = equal(3,6,-5)
+print("ค่ามากสุด => ",max)
 
 ########################################################################################################################
+
+def displayFood():
+    print("หูฉลาม")
+
+def displayCity():
+    displayFood()
+    print("สวัสดีกรุงเทพ")
+
+displayCity()
+
