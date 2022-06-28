e8fcd044df958584786edbc6eb8bdd0813f20307
Sumetjutha <sumet.jutha@gmail.com>
2022-06-28 19:25:37 +0700

    update

--- ep64.py
+++ ep64.py
@@ -1,9 +1,24 @@
-## 
-
+## ฟังก์ชันเช็๋คจำนวนตัวอักษร พิมพ์เล็ก/พิมพ์ใหญ่
+ 
 
 ########################################################################################################################
+# ABcDEfg
 
+def checkString(message):
+    result = {"UPPER":0,"LOWER":0}
+    for c in message:
+        if c.isupper():
+            result["UPPER"] += 1
+        elif c.islower():
+            result["LOWER"] += 1
+        else:
+            pass
+        # print(c," = ")
+    return result
 
+message = input("input your message : ")
 
+x = checkString(message)
+print(x)
 
 ########################################################################################################################
